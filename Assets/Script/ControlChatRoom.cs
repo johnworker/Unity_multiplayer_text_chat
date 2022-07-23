c74bf15e97e2647a1e400e320355f0c4bac37c70
johnworker <78348941+johnworker@users.noreply.github.com>
2022-07-23 22:34:45 +0800

    修正

--- Assets/Script/ControlChatRoom.cs
+++ Assets/Script/ControlChatRoom.cs
@@ -1,18 +1,40 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ControlChatRoom : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public InputField chatInput;
+    public TextMeshProUGUI chatText;
+    public ScrollRect scrollRect;
+    string username = "DHX";
+    // Use this for initialization
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            if (chatInput.text != "")
+            {
+                string addText = "\n  " + "<color=red>" + username + "</color>:\u00A0" + chatInput.text;
+                chatText.text += addText;
+                chatInput.text = "";
+                chatInput.ActivateInputField();
+                Canvas.ForceUpdateCanvases();
+                scrollRect.verticalNormalizedPosition = 0f;
+                Canvas.ForceUpdateCanvases();
+            }
+        }
+
     }
 }
