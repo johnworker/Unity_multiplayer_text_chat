06fe416c34e31722a11dd39b7312c6d27b23e138
johnworker <78348941+johnworker@users.noreply.github.com>
2022-07-25 13:07:50 +0800

    添加控制台應用程序

---
+++ Assets/FrameConsoleApp1/FirstConsoleApp/FirstConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstConsoleApp
+{
+    class Program
+    {
+
+        /// <summary>
+        /// 印出訊息
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            // 支援印出信息
+            Console.WriteLine("Hi, kane!");
+
+            Console.WriteLine("username");
+
+            Console.WriteLine("");
+
+            
+        }
+
+    }
+}
